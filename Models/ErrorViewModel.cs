021f04e79512832dbf39cf2e2cd4ba2d2bb44d79
Mevlüt Kutlu <mevlutkutlu2@gmail.com>
2024-12-16 16:09:24 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
